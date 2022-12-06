71d3564e5e17ae6921ec42c4f6b452276e254a6a
sunshine970918 <76559888+sunshine970918@users.noreply.github.com>
2022-12-06 14:54:58 +0800

    Create coolapk.js

---
+++ Module/Panel/Coolapk/coolapk.js
@@ -0,0 +1,43 @@
+// 酷安 replyList
+if (/^https?:\/\/api.coolapk.com\/v6\/feed\/replyList/.test(url)) {
+  let obj = JSON.parse(body);
+  obj.data = Object.values(obj.data).filter((item) => item.id);
+  body = JSON.stringify(obj);
+}
+
+// 酷安 index
+if (/^https?:\/\/api.coolapk.com\/v6\/main\/indexV8/.test(url)) {
+  let obj = JSON.parse(body);
+  obj.data = Object.values(obj.data).filter(
+    (item) =>
+      !(
+        item["entityTemplate"] == "sponsorCard" ||
+        item.entityId == 8639 ||
+        item.entityId == 33066 ||
+        item.entityId == 32557 ||
+        item.title.indexOf("值得买") != -1
+      )
+  );
+  body = JSON.stringify(obj);
+}
+
+// 酷安 dataList
+if (/^https?:\/\/api.coolapk.com\/v6\/main\/dataList/.test(url)) {
+  let obj = JSON.parse(body);
+  obj.data = Object.values(obj.data).filter(
+    (item) =>
+      !(item["entityTemplate"] == "sponsorCard" || item.title == "精选配件")
+  );
+  body = JSON.stringify(obj);
+}
+
+// 酷安 detail
+if (/^https?:\/\/api.coolapk.com\/v6\/feed\/detail/.test(url)) {
+  let obj = JSON.parse(body);
+  obj.data.hotReplyRows = Object.values(obj.data.hotReplyRows).filter(
+    (item) => item["id"]
+  );
+  obj.data.include_goods_ids = [];
+  obj.data.include_goods = [];
+  body = JSON.stringify(obj);
+}
