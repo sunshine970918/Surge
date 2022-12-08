1f03725311a30ac6a976b98e5cabf33074132419
sunshine970918 <76559888+sunshine970918@users.noreply.github.com>
2022-12-08 18:33:33 +0800

    Update coolapk.js

--- Module/Panel/Coolapk/coolapk.js
+++ Module/Panel/Coolapk/coolapk.js
@@ -5,21 +5,21 @@ if (/^https?:\/\/api.coolapk.com\/v6\/feed\/replyList/.test(url)) {
   body = JSON.stringify(obj);
 }
 
-// 酷安 index
-//if (/^https?:\/\/api.coolapk.com\/v6\/main\/indexV8/.test(url)) {
-//  let obj = JSON.parse(body);
-//  obj.data = Object.values(obj.data).filter(
-//    (item) =>
-//      !(
-//        item["entityTemplate"] == "sponsorCard" ||
-//        item.entityId == 8639 ||
-//        item.entityId == 33066 ||
-//        item.entityId == 32557 ||
-//        item.title.indexOf("值得买") != -1
-//      )
-//  );
-//  body = JSON.stringify(obj);
-///}
+ 酷安 index
+if (/^https?:\/\/api.coolapk.com\/v6\/main\/indexV8/.test(url)) {
+  let obj = JSON.parse(body);
+  obj.data = Object.values(obj.data).filter(
+    (item) =>
+      !(
+        item["entityTemplate"] == "sponsorCard" ||
+        item.entityId == 8639 ||
+        item.entityId == 33066 ||
+        item.entityId == 32557 ||
+        item.title.indexOf("值得买") != -1
+      )
+  );
+  body = JSON.stringify(obj);
+}
 
 // 酷安 dataList
 if (/^https?:\/\/api.coolapk.com\/v6\/main\/dataList/.test(url)) {
@@ -32,12 +32,12 @@ if (/^https?:\/\/api.coolapk.com\/v6\/main\/dataList/.test(url)) {
 }
 
 // 酷安 detail
-if (/^https?:\/\/api.coolapk.com\/v6\/feed\/detail/.test(url)) {
-  let obj = JSON.parse(body);
-  obj.data.hotReplyRows = Object.values(obj.data.hotReplyRows).filter(
-    (item) => item["id"]
-  );
-  obj.data.include_goods_ids = [];
-  obj.data.include_goods = [];
-  body = JSON.stringify(obj);
-}
+//if (/^https?:\/\/api.coolapk.com\/v6\/feed\/detail/.test(url)) {
+//  let obj = JSON.parse(body);
+//  obj.data.hotReplyRows = Object.values(obj.data.hotReplyRows).filter(
+//    (item) => item["id"]
+//  );
+//  obj.data.include_goods_ids = [];
+//  obj.data.include_goods = [];
+//  body = JSON.stringify(obj);
+//}
