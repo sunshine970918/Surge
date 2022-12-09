c95383921f89cda9dce7a7f9d00cde1065c64a2a
sunshine970918 <76559888+sunshine970918@users.noreply.github.com>
2022-12-09 23:48:10 +0800

    Update coolapk.js

--- Module/Panel/Coolapk/coolapk.js
+++ Module/Panel/Coolapk/coolapk.js
@@ -1,43 +1,30 @@
-// 酷安 replyList
-if (/^https?:\/\/api.coolapk.com\/v6\/feed\/replyList/.test(url)) {
-  let obj = JSON.parse(body);
-  obj.data = Object.values(obj.data).filter((item) => item.id);
-  body = JSON.stringify(obj);
-}
-
- 酷安 index
-if (/^https?:\/\/api.coolapk.com\/v6\/main\/indexV8/.test(url)) {
-  let obj = JSON.parse(body);
-  obj.data = Object.values(obj.data).filter(
-    (item) =>
-      !(
-        item["entityTemplate"] == "sponsorCard" ||
-        item.entityId == 8639 ||
-        item.entityId == 33066 ||
-        item.entityId == 32557 ||
-        item.title.indexOf("值得买") != -1
-      )
-  );
-  body = JSON.stringify(obj);
-}
-
- 酷安 dataList
-if (/^https?:\/\/api.coolapk.com\/v6\/main\/dataList/.test(url)) {
-  let obj = JSON.parse(body);
-  obj.data = Object.values(obj.data).filter(
-    (item) =>
-      !(item["entityTemplate"] == "sponsorCard" || item.title == "精选配件")
-  );
-  body = JSON.stringify(obj);
-}
-
- 酷安 detail
-if (/^https?:\/\/api.coolapk.com\/v6\/feed\/detail/.test(url)) {
-  let obj = JSON.parse(body);
-  obj.data.hotReplyRows = Object.values(obj.data.hotReplyRows).filter(
-    (item) => item["id"]
-  );
-  obj.data.include_goods_ids = [];
-  obj.data.include_goods = [];
-  body = JSON.stringify(obj);
+if ($request.url.indexOf("replyList") != -1) {
+    var bodyObj = JSON.parse($response.body);
+    bodyObj.data = Object.values(bodyObj.data).filter((item) => item.id);
+    $done({
+        body: JSON.stringify(bodyObj),
+    });
+} else if ($request.url.indexOf("indexV8") != -1) {
+    var bodyObj = JSON.parse($response.body);
+    bodyObj.data = Object.values(bodyObj.data).filter((item) => !(item["entityTemplate"] == "sponsorCard" || item.entityId == 8639 || item.entityId == 33006 || item.entityId == 32557 || item.title.indexOf("值得买") != -1));
+    //去除头条信息流推广和首页轮转
+    $done({
+        body: JSON.stringify(bodyObj),
+    });
+} else if ($request.url.indexOf("dataList") != -1) {
+    var bodyObj = JSON.parse($response.body);
+    bodyObj.data = Object.values(bodyObj.data).filter((item) => !(item["entityTemplate"] == "sponsorCard" || item.title == "精选配件"));
+    $done({
+        body: JSON.stringify(bodyObj),
+    });
+} else if ($request.url.indexOf("detail") != -1) {
+    var bodyObj = JSON.parse($response.body);
+    bodyObj.data.hotReplyRows = Object.values(bodyObj.data.hotReplyRows).filter((item) => item["id"]);
+    bodyObj.data.include_goods_ids = [];
+    bodyObj.data.include_goods = [];
+    $done({
+        body: JSON.stringify(bodyObj),
+    });
+} else {
+    $done($response);
 }
