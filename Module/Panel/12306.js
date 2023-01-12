588e4d08b839299e679dd835cbab73795539c58c
sunshine970918 <76559888+sunshine970918@users.noreply.github.com>
2023-01-12 18:17:11 +0800

    Create 12306.js

---
+++ Module/Panel/12306.js
@@ -0,0 +1,21 @@
+// 2023-01-06 18:00
+
+if (!$response.body) $done({});
+const url = $request.url;
+let obj = JSON.parse($response.body);
+
+if (url.includes("/ad.12306.cn/ad/ser/getAdList")) {
+  if (obj.materialsList) {
+    if (obj.advertParam && obj.materialsList.length === 1) {
+      obj.materialsList[0].billId = "1000005";
+      obj.materialsList[0].billMaterialsId = "2000005";
+      obj.materialsList[0].filePath = "";
+      obj.advertParam.skipTime = 1;
+    } else if (obj.materialsList.length > 1) {
+      obj.materialsList = [];
+    }
+  }
+}
+
+body = JSON.stringify(obj);
+$done({ body });
