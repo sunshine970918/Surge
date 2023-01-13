dbb91855f3bc5e8d39e67c04d4b687686e51d7a6
sunshine970918 <76559888+sunshine970918@users.noreply.github.com>
2023-01-13 11:03:32 +0800

    Update amap.js

--- Module/Panel/amap.js
+++ Module/Panel/amap.js
@@ -1,51 +1,121 @@
-// 2023-01-11 09:02
+// 2023-01-13 11:02
 
-if (!$response.body) $done({});
-const url = $request.url;
-let obj = JSON.parse($response.body);
-
-if (url.includes("/msgbox/pull")) {
-  // 高德地图-首页消息
-  if (obj.msgs) {
-    obj.msgs = [];
-  }
-} else if (obj.data) {
-  if (url.includes("/faas/amap-navigation/main-page")) {
-    // 高德地图-首页卡片
-    if (obj.data.cardList) {
-      obj.data.cardList = obj.data.cardList.filter((item) => {
-        return item.dataKey === "LoginCard";
-      });
+var url = $request.url
+var change = false
+var obj
+try {
+    obj = JSON.parse($response.body)
+} catch (e) {
+    console.log(e + "\n错误URL：" + url)
+    $done({})
+}
+if (url.includes("valueadded/alimama/splash_screen")) {  // 开屏广告
+    if (obj.data?.ad) {
+        for (const item of obj.data.ad) {
+            item.set.setting.display_time = 0
+            item.creative[0].start_time = 2240150400
+            item.creative[0].end_time = 2240150400
+        }
+        change = true
     }
-    // } else if (url.includes("/faas/amap-navigation/main-page-assets")) {
-    //   // 高德地图-首页消息
-    //   if (obj.data.pull3 && obj.data.pull3.msgs) {
-    //     obj.data.pull3.msgs.starttime = 2208960000; // Unix 时间戳 2040-01-01 00:00:00
-    //     obj.data.pull3.msgs.expiretime = 2209046399; // Unix 时间戳 2040-01-01 23:59:59
-    //   }
-  } else if (url.includes("/shield/dsp/profile/index/nodefaasv3")) {
-    // 高德地图-我的
-    if (obj.data.cardList) {
-      obj.data.cardList = obj.data.cardList.filter((item) => {
-        return (item.dataKey === "MyOrderCard");
-      });
+} else if (url.includes("faas/amap-navigation/main-page")) {  // 首页底栏
+    if (obj.data?.cardList) {
+        obj.data.cardList = Object.values(obj.data.cardList.filter(item => {
+            return item.dataType === "LoginCard"
+        }))
+        change = true
     }
-  } else if (url.includes("/shield/search/new_hotword")) {
-    // 高德地图-搜索框
-    if (obj.data.header_hotword) {
-      obj.data.header_hotword = [];
+    if (obj.data?.pull3?.msgs) {
+        obj.data.pull3.msgs = []
+        change = true
     }
-  } else if (url.includes("/valueadded/alimama/splash_screen")) {
-    // 高德地图-开屏广告
-    if (obj.data.ad) {
-      for (let item of obj.data.ad) {
-        item.set.setting.display_time = 0;
-        item.creative[0].start_time = 2208960000; // Unix 时间戳 2040-01-01 00:00:00
-        item.creative[0].end_time = 2209046399; // Unix 时间戳 2040-01-01 23:59:59
-      }
+    if (obj.data?.mapBizList) {
+        obj.data.mapBizList = []
+        change = true
     }
-  }
+} else if (url.includes("dsp/profile/index/nodefaas")) {  // 我的页面
+    obj.data.tipData = undefined
+    if (obj.data?.cardList) {
+        obj.data.cardList = Object.values(obj.data.cardList.filter(item => {
+            return item.dataType === "MyOrderCard" || item.dataType === "GdRecommendCard"
+            // || item.dataKey === "SceneVehicleCard_function"  // 我的车辆
+            // || item.dataKey === "AnnualBillCardV2"           // 年度报告
+            // || item.dataKey === "PopularActivitiesCard"      // 热门活动
+            // || item.dataKey === "GameExcitation"             // 小德爱消除
+            // || item.dataKey === "GoodsShelvesCard"           // 精选服务
+            // || item.dataKey === "DiyMap_function"            // DIY 地图
+        }))
+        change = true
+    }
+} else if (url.includes("search/new_hotword")) {  // 热词
+    if (obj.data?.header_hotword) {
+        obj.data.header_hotword = []
+        change = true
+    }
+} else if (url.includes("ws/msgbox/pull")) {  // 首页顶部横幅
+    if (obj.msgs) {
+        obj.msgs = []
+        change = true
+    }
+    if (obj.pull3?.msgs) {
+        obj.pull3.msgs = []
+        change = true
+    }
+} else if (url.includes("ws/promotion-web/resource")) {  // 打车页面
+    if (obj.data?.icon) {
+        obj.data.icon = undefined
+        change = true
+    }
+    if (obj.data?.tips) {
+        obj.data.tips = undefined
+        change = true
+    }
+    if (obj.data?.popup) {
+        obj.data.popup = undefined
+        change = true
+    }
+    if (obj.data?.banner) {
+        obj.data.banner = undefined
+        change = true
+    }
+    if (obj.data?.bubble) {
+        Object.keys(obj.data.bubble).forEach(key => {
+            obj.data.bubble[key] = []
+        })
+        change = true
+    }
+} else if (url.includes("search/nearbyrec_smart")) {  // 附近页面
+    // if (obj.data?.coupon) {
+    //     obj.data.coupon = undefined
+    //     change = true
+    // }
+    // if (obj.data?.scene) {
+    //     obj.data.scene = undefined
+    //     change = true
+    // }
+    // if (obj.data?.activity) {
+    //     obj.data.activity = undefined
+    //     change = true
+    // }
+    // if (obj.data?.commodity_rec) {
+    //     obj.data.commodity_rec = undefined
+    //     change = true
+    // }
+    if (obj.data?.modules) {
+        obj.data.modules = obj.data.modules.filter(item => {
+            return item !== "coupon" &&        // coupon 右下角广告
+                // item !== "scene" &&            // 不知道对应啥
+                // item !== "activity" &&         // 不知道对应啥
+                item !== "commodity_rec"       // commodity_rec 超值套餐
+        })
+        change = true
+    }
+} else {
+    console.log('触发意外的请求，请确认脚本或复写配置是否正常\n错误URL：' + url)
 }
 
-body = JSON.stringify(obj);
-$done({ body });
+if (change) {
+    $done({body: JSON.stringify(obj)})
+} else {
+    $done({})
+}
